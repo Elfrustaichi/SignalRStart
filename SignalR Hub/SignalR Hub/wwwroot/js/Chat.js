65a35a71748b809102a5cc84bbdd094725332cb5
umidnm <umidnm@code.edu.az>
2023-06-01 17:18:41 +0400

    SignalR added

---
+++ SignalR Hub/SignalR Hub/wwwroot/js/Chat.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+connection.start(); 
+
+
+$(document).on("click", "#sendButton", function (e) {
+    let user = $("#userInput").val();
+    let message = $("#messageInput").val();
+
+    connection.invoke("SendMessage", user, message);
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user} says ${message}`;
+});
